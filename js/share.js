dcebedf95eef2a5ebd75352b80f0701c1ada084a
M.cq <c_q_gg@163.com>
2019-01-23 19:23:29 +0800

    combine before

--- js/share.js
+++ js/share.js
@@ -1,15 +1,29 @@
  $('a').attr('href','javascript:void(0)')
  $('.NIE-share-iconBtn>li').click(function () {
-     console.log(1)
+    let index=$(this).index();
+    let btn=$('.right').find(':submit');
+    let btnText;
+    if(index===0)btnText="易信登录";
+    if(index===1)btnText="微信登录";
+    if(index===2)btnText="微博登录";
+    if(index===3)btnText="空间登录";
+    btn.val(btnText);
      $(".popup").show();
-     $(".layer").show().css('width',$('#out-bg').width()); //查找ID为popup的DIV show()显示#gray
+     $(".layer").show().css('width',$('#out-bg').width()); 
  });
  //点击关闭按钮
  $("a.guanbi").click(function () {
-     $(".layer").hide(); //查找ID为popup的DIV hide()隐藏
+     $(".layer").hide(); 
  })
      $(document).mouseup(function () {
          $(".popup").css("cursor", "default");
          $(this).unbind("mousemove");
      });
- 
+
+// 创建单利
+function Single(){
+
+ }
+ Single.prototype.createSingle=function(){
+    let el=$(``)
+ }
